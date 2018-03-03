f6c56f46cd70d4cca9a12697e1d89c03a744b1ed
Matias Cecchetto <matias.cec@gmail.com>
2018-03-03 23:35:11 +1000

    Test

--- controllers/api_ctrler.js
+++ controllers/api_ctrler.js
@@ -3,7 +3,7 @@ var models = require('../models/loader.js');
 //GET /api/all
 exports.all = function(req, res) {
 
-  console.log('Environment:' + JSON.stringify(process.env.REACT_APP_URL));
+  console.log('Environment:' + process.env.REACT_APP_URL);
 
   models.apicomments.findAll().then(
     function(comments) {
