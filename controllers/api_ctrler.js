e15dcf23ba76d9b1ecd66fc1d4f248d3943fbfa3
Matias Cecchetto <matias.cec@gmail.com>
2018-03-03 19:59:18 +1000

    Added log to determine Heroku URL

--- controllers/api_ctrler.js
+++ controllers/api_ctrler.js
@@ -2,6 +2,12 @@ var models = require('../models/loader.js');
 
 //GET /api/all
 exports.all = function(req, res) {
+  console.log(req.get('host'));
+  // console.log(origin);
+  // res.header('Access-Control-Allow-Origin', origin);
+  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+
   models.apicomments.findAll().then(
     function(comments) {
       res.json(comments);
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,13 @@ var router = express.Router();
 var apiCtrler = require('../controllers/api_ctrler');
 var cors = require('cors');
 
+// var corsOptions = {
+//   origin: 'react-app-mc.herokuapp.com/:1',
+//   methods: 'GET',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+
+// router.get('/all', cors(corsOptions), apiCtrler.all);
 router.get('/all', cors(), apiCtrler.all);
 
 module.exports = router;
