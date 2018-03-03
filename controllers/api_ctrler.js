feb12f8fd42bd3eae07b8a8befe2ccb72e9d710a
Matias Cecchetto <matias.cec@gmail.com>
2018-03-03 20:53:24 +1000

    Get origin

--- controllers/api_ctrler.js
+++ controllers/api_ctrler.js
@@ -2,11 +2,8 @@ var models = require('../models/loader.js');
 
 //GET /api/all
 exports.all = function(req, res) {
-  console.log('The host is: '+ req.get('host'));
-  // console.log(origin);
-  // res.header('Access-Control-Allow-Origin', origin);
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+
+  console.log(req.headers.origin);
 
   models.apicomments.findAll().then(
     function(comments) {
