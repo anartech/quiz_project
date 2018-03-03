893044c4d54d4e66c26ebf04845e57dc93ea7156
Matias Cecchetto <matias.cec@gmail.com>
2018-03-03 20:04:18 +1000

    Stupid me

--- controllers/api_ctrler.js
+++ controllers/api_ctrler.js
@@ -2,7 +2,7 @@ var models = require('../models/loader.js');
 
 //GET /api/all
 exports.all = function(req, res) {
-  console.log(The host is: req.get('host'));
+  console.log('The host is: '+ req.get('host'));
   // console.log(origin);
   // res.header('Access-Control-Allow-Origin', origin);
   // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
